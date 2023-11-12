9556f773584b5135b29aa4aa3d1503099204f749
Mirko Adzic <adzicmirko97@gmail.com>
2023-11-12 21:00:35 +0100

    Added new dunder methods for Value class
    Added:
    - subtraction, division and negation methods
    - exponentiation and sigmoid functions
    - corresponding tests

--- minigrad.py
+++ minigrad.py
@@ -1,53 +1,90 @@
+import math
+
 class Value:
-    def __init__(self, val: float) -> None:
-        self.val = val
+    def __init__(self, data: float) -> None:
+        self.data = data
+
+    def item(self):
+        return self.data
 
     def __add__(self, other):
         if isinstance(other, Value):
-            out = Value(self.val + other.val)
+            out = Value(self.data + other.data)
         else:
-            out = Value(self.val + other)
+            out = Value(self.data + other)
         
         return out
     
     def __radd__(self, other):
+        out = Value(other + self.data)
+        return out
+
+    def __sub__(self, other):
         if isinstance(other, Value):
-            out = Value(self.val + other.val)
+            out = Value(self.data - other.data)
         else:
-            out = Value(self.val + other)
+            out = Value(self.data - other)
         
         return out
     
+    def __rsub__(self, other):
+        out = Value(other - self.data)
+        return out
+
+    def __neg__(self):
+        out = Value(-self.data)
+        return out
+
     def __mul__(self, other):
         if isinstance(other, Value):
-            out = Value(self.val * other.val)
+            out = Value(self.data * other.data)
         else:
-            out = Value(self.val * other)
+            out = Value(self.data * other)
 
         return out
     
     def __rmul__(self, other):
+        out = Value(other * self.data)
+        return out
+    
+    def __truediv__(self, other):
         if isinstance(other, Value):
-            out = Value(self.val * other.val)
+            out = Value(self.data / other.data)
         else:
-            out = Value(self.val * other)
+            out = Value(self.data / other)
 
+        return out
+    
+    def __rtruediv__(self, other):
+        out = Value(other / self.data)
         return out
     
     def __pow__(self, other):
         if isinstance(other, Value):
-            out = Value(self.val ** other.val)
+            out = Value(self.data ** other.data)
         else:
-            out = Value(self.val ** other)
+            out = Value(self.data ** other)
         
         return out
     
+    def __rpow__(self, other):
+        out = Value(other ** self.data)
+        return out
+    
+    def exp(self):
+        out = Value(math.e ** self.data)
+        return out
+
+    def sigmoid(self):
+        out = 1 / (1 + (-self).exp())
+        return out
+
     def relu(self):
-        out = Value(self.val if self.val > 0 else 0)
+        out = Value(self.data if self.data > 0 else 0)
         return out
     
     def __repr__(self) -> str:
-        return f"Value: {self.val}"
+        return f"Value: {self.data}"
     
 a = Value(3.0)
 b = Value(5.0)
@@ -55,8 +92,9 @@ b = Value(5.0)
 c = a + b
 d = c + 5.0
 e = 10.0 + d
-f = 150.0 + e
+f = 15.0 + e
 g = b ** a
 h = b ** 2
 i = a.relu()
 print(i)
+a = 3.0
--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 import unittest
+import math
 
-from mymodule import Value
+from minigrad import Value
 
 class TestValue(unittest.TestCase):
     def test_add_two_value_objects(self):
@@ -8,38 +9,126 @@ class TestValue(unittest.TestCase):
         b = 1.5
         v1 = Value(a)
         v2 = Value(b)
-        self.assertEqual((v1 + v2).val, a + b)
+        self.assertEqual((v1 + v2).item(), a + b)
 
     def test_add_value_object_and_scalar(self):
         a = 3.0
         b = 1.5
         v1 = Value(a)
-        self.assertEqual((v1 + b).val, a + b)
+        self.assertEqual((v1 + b).item(), a + b)
 
     def test_add_scalar_and_value_object(self):
         a = 3.0
         b = 1.5
         v1 = Value(a)
-        self.assertEqual((b + v1).val, a + b)
+        self.assertEqual((b + v1).item(), a + b)
+
+    def test_sub_two_value_objects(self):
+        a = 3.0
+        b = 1.5
+        v1 = Value(a)
+        v2 = Value(b)
+        self.assertEqual((v1 - v2).item(), a - b)
+
+    def test_sub_value_object_and_scalar(self):
+        a = 3.0
+        b = 1.5
+        v1 = Value(a)
+        self.assertEqual((v1 - b).item(), a - b)
+
+    def test_sub_scalar_and_value_object(self):
+        a = 3.0
+        b = 1.5
+        v1 = Value(a)
+        self.assertEqual((b - v1).item(), b - a)
+
+    def test_neg_value_object(self):
+        a = 3.0
+        v1 = Value(a)
+        self.assertEqual(-v1.item(), -a)
 
     def test_mul_two_value_objects(self):
         a = 3.0
         b = 1.5
         v1 = Value(a)
         v2 = Value(b)
-        self.assertEqual((v1 * v2).val, a * b)
+        self.assertEqual((v1 * v2).item(), a * b)
 
     def test_mul_value_object_and_scalar(self):
         a = 3.0
         b = 1.5
         v1 = Value(a)
-        self.assertEqual((v1 * b).val, a * b)
+        self.assertEqual((v1 * b).item(), a * b)
 
     def test_mul_scalar_and_value_object(self):
         a = 3.0
         b = 1.5
         v1 = Value(a)
-        self.assertEqual((b * v1).val, a * b)
+        self.assertEqual((b * v1).item(), a * b)
+
+    def test_div_two_value_objects(self):
+        a = 3.0
+        b = 1.5
+        v1 = Value(a)
+        v2 = Value(b)
+        self.assertEqual((v1 / v2).item(), a / b)
+
+    def test_div_value_object_and_scalar(self):
+        a = 3.0
+        b = 1.5
+        v1 = Value(a)
+        self.assertEqual((v1 / b).item(), a / b)
+
+    def test_div_scalar_and_value_object(self):
+        a = 3.0
+        v1 = Value(a)
+        self.assertEqual((1 / v1).item(), 1 / a)
+
+    def test_pow_two_value_objects(self):
+        a = 3.0
+        b = 2.0
+        v1 = Value(a)
+        v2 = Value(b)
+        self.assertEqual((v1 ** v2).item(), a ** b)
+
+    def test_pow_value_object_and_scalar(self):
+        a = 3.0
+        b = 2.0
+        v1 = Value(a)
+        self.assertEqual((v1 ** b).item(), a ** b)
+
+    def test_pow_scalar_and_value_object(self):
+        a = 3.0
+        b = 2.0
+        v1 = Value(a)
+        self.assertEqual((b ** v1).item(), b ** a)
+    
+    def test_exp(self):
+        a = 3.0
+        v1 = Value(a)
+        self.assertEqual((math.e ** v1).item(), math.e ** a)
+        
+    def test_sigmoid(self):
+        a = 3.0
+        v1 = Value(a)
+        self.assertEqual((v1.sigmoid()).item(), 1 / (1 + math.e ** -a))
+
+    def test_sigmoid(self):
+        a = 3.0
+        v1 = Value(a)
+        self.assertEqual((v1.sigmoid()).item(), 1 / (1 + math.e ** -a))
+
+    def test_relu_below_zero(self):
+        a = -3.0
+        a_relu = 0
+        v1 = Value(a)
+        self.assertEqual((v1.relu()).item(), a_relu)
+    
+    def test_relu_above_zero(self):
+        a = 3.0
+        a_relu = a
+        v1 = Value(a)
+        self.assertEqual((v1.relu()).item(), a_relu)
 
 if __name__ == "__main__":
     unittest.main()
