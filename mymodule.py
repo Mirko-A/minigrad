f7d6f99219cb4116ee056d22ea6c8e6c9bc1a876
Mirko Adzic <adzicmirko97@gmail.com>
2023-11-12 18:15:53 +0100

    Added __add__ and __radd__

--- mymodule.py
+++ mymodule.py
@@ -3,7 +3,39 @@ class Value:
         self.val = val
 
     def __add__(self, other):
-        return Value(self.val + other.val)
+        if isinstance(other, Value):
+            out = Value(self.val + other.val)
+        else:
+            assert isinstance(other, float)
+            out = Value(self.val + other)
+        
+        return out
+    
+    def __radd__(self, other):
+        if isinstance(other, Value):
+            out = Value(self.val + other.val)
+        else:
+            assert isinstance(other, float)
+            out = Value(self.val + other)
+        
+        return out
+    
+    # TODO: ----------------------------------------
+    # multiplication
+    # exponentiation
+    # sigmoid
+    # relu
+    # ----------------------------------------------
+    
     
     def __repr__(self) -> str:
         return f"Value: {self.val}"
+    
+a = Value(3.0)
+b = Value(5.0)
+
+c = a + b
+d = c + 5.0
+e = 10.0 + d
+f = 150.0 + e
+print(f)
