bbb479d6d8f3b15eed9b9039129c954c34428649
Mirko Adzic <adzicmirko97@gmail.com>
2023-12-30 16:15:16 +0100

    Performance example added

--- minitorch/playground.py
+++ minitorch/playground.py
@@ -3,7 +3,6 @@ from minitorch.nn.module import Sequence
 from minitorch.nn.optim import Adam
 from minitorch.nn.module import Sigmoid, Relu, Tanh, MSELoss, CrossEntropyLoss
 from minitorch.tensor import Tensor
-from minitorch.buffer import MiniBuffer
 
 import time
 
@@ -18,20 +17,20 @@ import cpp_backend
 
 # 1 1 1
 
-def main1():
+def main():
     start = time.time()
 
-    t = Tensor.arange(0, 8, True).reshape([1, 1, 8])
+    t1 = Tensor.arange(0, 2*64*64, True).reshape([2, 64, 64])
+    t1 = t1 / 1000
+    t2 = Tensor.arange(0, 2*64*64, True).reshape([2, 64, 64])
+    t2 = t2 / 1000
     #print(t)
     #t2 = Tensor.arange(0, 16).reshape([2, 2, 2, 2])
 
-    #print(t)
-    t2 = t.expand([2, 2, 8])
-    print(t2)
-    t3 = t2.sum()
-    print(t3)
-    t3.backward()
+    for _ in range(100):
+        y = t1 @ t2
 
+    print(y)
     print(f"Time: {time.time() - start}")
 
 def main0():
@@ -40,7 +39,7 @@ def main0():
     x = t.sum(0)
     print(x)
 
-def main():
+def main1():
     prki_net = Sequence(
         Linear(2, 4),
         Linear(4, 2),
